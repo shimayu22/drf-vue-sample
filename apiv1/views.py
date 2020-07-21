e54c3d12396208e533e550fce5aedebcdb14398b
Yutaka Shimada <shimayu22@gmail.com>
2020-07-21 17:02:12 +0900

    バックエンド構築

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from shop.models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
