a507c96a2bc0d2afb2611b64b1929ea7d5743720
Yutaka Shimada <shimayu22@gmail.com>
2020-09-17 10:57:15 +0900

    App.vueを編集

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+  }
+
+  main.container {
+    border: 1px solid #cccccc;
+    margin-top: 4em;
+    padding: 2em;
+    width: 600px;
+  }
+
 </style>
