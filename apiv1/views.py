b04e18f02d3e3d69e40d54129754146c5f9978f2
Yutaka Shimada <shimayu22@gmail.com>
2020-09-17 10:16:33 +0900

    10.2まで見直し修正

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
+import uuid
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Book(models.Model):
+    class Meta:
+        db_table = 'book'
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(verbose_name="タイトル", max_length=20)
+    price = models.IntegerField(verbose_name="価格", null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
